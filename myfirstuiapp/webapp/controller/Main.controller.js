a7d9830a1bb589753416b939e6dea568a2a5fcbd
Subramaniyam N <subramaniyam96@gmail.com>
2025-10-10 01:38:20 +0000

    basics of model

--- myfirstuiapp/webapp/controller/Main.controller.js
+++ myfirstuiapp/webapp/controller/Main.controller.js
@@ -1,10 +1,45 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller,
+	JSONModel) {
     "use strict";
 
     return Controller.extend("mickey.controller.Main", {
+        oView: null,
+          onInit:function(){
+            this.oView = this.getView()
+            var oModel = new sap.ui.model.json.JSONModel();
+            const data = {
+                "empStr":{
+                    "empId":500,
+                    "empName":"anubhav",
+                    "salary":11000,
+                    "currency":"USD",
+                    "smoker": false
+                },
+                "empTable":[
+                    {
+                    "empId":5010,
+                    "empName":"Bilal",
+                    "salary":1500,
+                    "currency":"EURO",
+                    "smoker": true
+                },
+                {
+                     "empId":50220,
+                    "empName":"Anany",
+                    "salary":112500,
+                    "currency":"EURO",
+                    "smoker": false
+                }
+                ]
+            }
+            oModel.setData(data);
 
+            sap.ui.getCore().setModel(oModel)
+            console.log(sap.ui.getCore().getModel())
+        },
         myCode:function(){
 
             var oInput = sap.ui.getCore().byId("idInp")
@@ -16,13 +51,30 @@ sap.ui.define([
             oInput.setValue("Hellow world !!")
         },
         onClickButton:function(){
-             var oView = this.getView();
-             var oInp = oView.byId("idXmlField");
+            
+             var oInp = this.oView.byId("idXmlField");
 
              var sValue = oInp.getValue();
              alert(sValue)
 
+        },
+        onPrintData:function(){
+          
+            var oEmpid = this.oView.byId("idEmpId");
+            var oEmpName = this.oView.byId("idEmpName");
+            var oSalary = this.oView.byId("idSal");
+            var oCurrency = this.oView.byId("idCurr");
+            var oSmoker = this.oView.byId("idSmoker");
+          
+            var sEmpid = oEmpid.getValue()
+            var sEmpName = oEmpName.getValue()
+            var sSalary = oSalary.getValue()
+            var sCurrency = oCurrency.getValue()
+            var sSmoker = oSmoker.getSelected()
+
+            alert(`${sEmpid} ${sEmpName} ${sSalary} ${sCurrency} ${sSmoker}`)
         }
+      
 
     });
 });
